78a9f02d8f2a01e36567c2cd1792c3d82c07de7b
Frank Shearar <frank.shearar@gmail.com>
2012-11-10 22:17:53 +0000

    First pass at defn-fix. It's probably broken because I'm not using dynamic variables. I'll wait for an actual problem to manifest though...

--- src/clj_derp/core.clj
+++ src/clj_derp/core.clj
@@ -1,20 +1,68 @@
-(ns clj-derp.core)
+(ns clj-derp.core
+  (:require [clojure.set :as set]))
+
+(defmacro defn-fix [name bottom fn]
+  `(let* [*cache*# (atom {})
+       *changed?*# (atom false)
+       *running?*# (atom false)
+       *visited*# (atom {})]
+      (defn parse-null [x#]
+        ;(prn x#)
+        (let [cached?# (contains? (deref *cache*#) x#)
+              cached# (get (deref *cache*#) x#)
+              run?# (deref *running?*#)]
+          (cond
+           (and cached?# (not run?#))
+           cached#
+           (and run?# (get (deref *visited*#) x#))
+           (if cached?# cached# {})
+           run?#
+           (do
+             (swap! *visited*# assoc x# true)
+             (let [new-val# (parse-null-int x#)]
+               (when (not (= new-val# cached#))
+                 (swap! *changed?*# (fn [_#] true))
+                 (swap! *cache*# assoc x# new-val#))
+               new-val#))
+           (and (not cached?#) (not run?#))
+           (do
+             (swap! *changed?*# (fn [_#] true))
+             (swap! *running?*# (fn [_#] true))
+             (swap! *visited*# (fn [_#] {}))
+             (let [v# (atom {})]
+               (while (deref *changed?*#)
+                 (swap! *changed?*# (fn [_#] false))
+                 (swap! *visited*# (fn [_#] {}))
+                 (swap! v# (fn [_#] (parse-null-int x#))))
+               (deref v#))))))))
 
 (defprotocol Parser
-  (parse-null [this]))
+  (parse-null-int [this]))
+
+(defn-fix parse-null {} (fn [parser] parse-null-int parser))
 
 (defrecord empty-parser []
   Parser
-  (parse-null [_] #{}))
+  (parse-null-int [_] #{}))
 (defrecord empty-string-parser [treeSet]
   Parser
-  (parse-null [_] treeSet))
+  (parse-null-int [_] treeSet))
 (defrecord literal-parser [token]
   Parser
-  (parse-null [_] #{}))
+  (parse-null-int [_] #{}))
+(defrecord union-parser [left right]
+  Parser
+  (parse-null-int [p]
+    (set/union
+     (parse-null-int (force (:left p)))
+     (parse-null-int (force (:right p))))))
 
-; Utility constructors
+;; Utility constructors
 (defn empty-p [] (empty-parser.))
 (defn eps [] (empty-string-parser. #{nil}))
 (defn eps* [token] (empty-string-parser. #{token}))
-(defn literal [token] (literal-parser. token))
+(defn lit [token] (literal-parser. token))
+(defn alt
+  ([] (empty-p))
+  ([a & parsers]
+     (union-parser. (delay a) (delay (apply alt parsers)))))
--- test/clj_derp/core_test.clj
+++ test/clj_derp/core_test.clj
@@ -2,9 +2,17 @@
   (:use clojure.test
         clj-derp.core))
 
-(deftest parsing-null
-  (testing "Parse null"
+(deftest making-parsers
+  (testing "Making parsers"
+    (is (= (empty-p) (alt)))
+;    (is (= (lit "it") (alt (lit "it"))))
+    (is (= (lit "left") (force (:left (alt (lit "left") (lit "right"))))))))
+
+ (deftest parsing-null
+   (testing "Null parses"
     (is (= #{nil} (parse-null (eps))))
     (is (= #{} (parse-null (empty-p))))
-    (is (= #{} (parse-null (literal "a"))))
-    (is (= #{"a"} (parse-null (eps* "a"))))))
+    (is (= #{} (parse-null (lit "a"))))
+    (is (= #{"a"} (parse-null (eps* "a"))))
+     (testing "of Union"
+       (is (= #{"a" "b"} (parse-null (alt (eps* "a") (eps* "b"))))))))
